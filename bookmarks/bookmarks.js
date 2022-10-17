c8512d0e7f7c9d487437afd380ff2960be81fad6
dibbla <470881710@qq.com>
2022-10-17 15:39:49 +0800

    adding arXiv restriction

--- bookmarks/bookmarks.js
+++ bookmarks/bookmarks.js
@@ -1,8 +1,18 @@
 function generate_content(key,url){
     var arXiv = /arxiv\.org\/abs\/[0-9]{4}\.[0-9]{5}/
+    var arXiv_pdf = /arxiv\.org\/pdf\/[0-9]{4}\.[0-9]{5}\.pdf/
+
+    console.log("Now checking: ",url)
 
     if(arXiv.test(url)){
-        document.body.innerHTML += "<a href=\"" + val + "\">" +key + "</a>" + "<br"
+        document.body.innerHTML += "<p><a href=\"" + url + "\">" +key + "</a></p>" + "<br>"
+        console.log("1st passed")
+    }
+    else{
+        if(arXiv_pdf.test(url)){
+            document.body.innerHTML += "<p><a href=\"" + url + "\">" + "[pdf]" +key + "</a></p>" + "<br>"
+            console.log("2nd passed")
+        }
     }
 }
 
@@ -10,10 +20,7 @@ function list_all_bookmarks(){
     var content = ""
     chrome.storage.sync.get(null, function(all){
         for([key, val] of Object.entries(all)){
-            // console.log(val)
             generate_content(key,val)
-
-
         }
     })
 }
