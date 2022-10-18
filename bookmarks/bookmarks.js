7b1baaf884a583f820f6de6365b11ec62c971f4c
dibbla <470881710@qq.com>
2022-10-18 22:16:28 +0800

    add pdf-url convert to title

--- bookmarks/bookmarks.js
+++ bookmarks/bookmarks.js
@@ -2,9 +2,26 @@ function arXiv_to_arXiv_pdf(url){
     return "arxiv.org/pdf/" + url.substring(22) + ".pdf"
 }
 
-// function arXiv_pdf_to_arXiv(url){
-    // request for title
-// }
+function arXiv_pdf_to_arXiv(url){
+    var api_base = "http://export.arxiv.org/api/query?search_query=id:" + url.substring(22,32)
+    // console.log(api_base)
+    try{
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", api_base, false ); // false for synchronous request
+        xmlHttp.onerror = function(){return result.substring(title_start_index,title_end_index)}
+        xmlHttp.send( null );
+        // console.log(stat)
+        var result = xmlHttp.responseText
+        var title_start_index = result.indexOf("<title>") + 7
+        var title_end_index = result.lastIndexOf("</title>")
+        // console.log("Finding: ",title_start_index,title_end_index,result.substring(title_start_index,title_end_index))
+
+        return result.substring(title_start_index,title_end_index)
+    }
+    catch(e){
+        return url.substring(22,32)
+    }
+}
 
 function generate_content(info){
     var arXiv = /arxiv\.org\/abs\/[0-9]{4}\.[0-9]{5}/
@@ -17,7 +34,8 @@ function generate_content(info){
         console.log("1st passed")
     }
     if(arXiv_pdf.test(info[1][0])){
-        document.body.innerHTML += "<p><a href=\"" + info[1][0] + "\">" + "[pdf]" +info[0] + "</a></p>" + "<br>"
+        document.body.innerHTML += "<p><a href=\"" + info[1][0] + "\">" + "[PDF]" +arXiv_pdf_to_arXiv(info[1][0]) + "</a></p>" + "<br>"
+        arXiv_pdf_to_arXiv(info[1][0])
         console.log("2nd passed")
     }
 }
